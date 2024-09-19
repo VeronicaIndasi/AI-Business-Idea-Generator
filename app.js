553eb6133d6e675422e794dab21755276653aa9e
VeronicaIndasi <vee.indasi@gmail.com>
2024-09-19 13:56:05 +0300

    Modified app.js file

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Serve static files (like your index.html)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Import routes
 const ideaRoutes = require('./routes/ideas');
 app.use('/api/ideas', ideaRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3300;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
